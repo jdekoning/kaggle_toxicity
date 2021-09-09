fcb87bda422ba1e05549a9670f8807c4dfca5941
Jaap de Koning <jaap.de.koning@bigdatarepublic.nl>
2021-09-09 23:45:35 +0200

    fix: updated anaconda and target bucket

--- utils/s3.py
+++ utils/s3.py
@@ -7,5 +7,5 @@ class S3Utils:
         call("cp /data/config /root/.aws/.".split(sep=' '))
         call("cp /data/credentials /root/.aws/.".split(sep=' '))
 
-        aws_cmd = "aws s3 cp --recursive /ekholabs/toxicity/model_output/%s s3://ekholabs-kaggle-models" % model_type
+        aws_cmd = "aws s3 cp --recursive /ekholabs/toxicity/model_output/%s s3://toxicity-models" % model_type
         call(aws_cmd.split(sep=' '))
